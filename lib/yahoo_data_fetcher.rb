76b65008ea79841bd7369768aef980321774ff83
Doug de Jesus <douglasdejesus92@gmail.com>
2020-11-17 18:35:52 -0500

    Add data fetcher for PlayerHistory

--- lib/yahoo_data_fetcher.rb
+++ lib/yahoo_data_fetcher.rb
@@ -2,6 +2,7 @@
 
 require_relative './yahoo_data_fetcher/draft_results'
 require_relative './yahoo_data_fetcher/game_results'
+require_relative './yahoo_data_fetcher/player_history'
 require_relative './yahoo_data_fetcher/teams'
 require_relative './yahoo_data_fetcher/weekly_roster_stats'
 
---
+++ lib/yahoo_data_fetcher/player_history.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'net/http'
+
+module YahooDataFetcher
+  class PlayerHistory
+    attr_reader :data
+    def initialize(player_id)
+      @data = fetch_player_history(player_id)
+    end
+
+    def fetch_player_history(player_id)
+      response = Net::HTTP.get_response(URI.parse("https://football.fantasysports.yahoo.com/f1/810182/playernote?init=0&view=history&pid=#{player_id}"))
+      doc = Nokogiri::HTML(JSON.parse(response.body)['content'])
+      rows = doc.css('tr')[1..]
+      rows.map do |row|
+        {
+          date: row.children[1].text,
+          event: row.children[3].text
+        }
+      end
+    end
+
+    def most_recent_event
+      @data.first[:event]
+    end
+
+    def acquired_in_trade?
+      most_recent_event.start_with?('Traded')
+    end
+  end
+end
