34ab404ed9e12a8326451506a4317abb06fb30c7
Doug de Jesus <douglasdejesus92@gmail.com>
2020-12-24 13:12:05 -0800

    Week 15 challenge and results

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source 'https://rubygems.org'
 ruby '~> 2.6.5'
 
 gem 'activesupport'
+gem 'descriptive-statistics'
 gem 'json'
 gem 'nokogiri'
 gem 'open-uri'
---
+++ week15.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+require 'bundler/setup'
+Bundler.require(:default)
+
+require_relative 'lib/yahoo_data_fetcher.rb'
+
+challenge_week = 15
+team_results = []
+
+teams = YahooDataFetcher::Teams.new
+
+(1..YahooDataFetcher::Teams::NUM_TEAMS).each do |team_index|
+  team_name = teams.index_to_name(team_index)
+  puts("************** #{team_name} **************")
+
+  scores = []
+
+  (1..13).each do |week|
+    game_result = YahooDataFetcher::GameResults.fetch_game_results(week, team_index)
+    scores << game_result[:team_1_pts]
+  end
+
+  stats = DescriptiveStatistics::Stats.new(scores)
+  tp([{
+    avg: stats.mean.round(2),
+    median: stats.median,
+    min: stats.min,
+    max: stats.max,
+    range: stats.range,
+    std_dev: stats.standard_deviation.round(2)
+  }])
+
+  challenge_week_result = YahooDataFetcher::GameResults.fetch_game_results(challenge_week, team_index)
+  challenge_week_pts = challenge_week_result[:team_1_pts]
+
+  puts("\nWeek 15: #{challenge_week_pts} points \n\n")
+
+  team_results << {
+    team_name: team_name,
+    avg_pts: stats.mean.round(2),
+    week_15_pts: challenge_week_pts,
+    difference: (challenge_week_pts - stats.mean).round(2)
+  }
+
+  sleep(10) # Try not to exceed rate limit
+end
+
+puts('************** RESULTS **************')
+team_results = team_results.sort_by { |data| -data[:difference] }.each.with_index do |data, i|
+  data[:rank] = i + 1
+  data
+end
+
+tp(team_results, :rank, :team_name, :week_15_pts, :avg_pts, :difference)
