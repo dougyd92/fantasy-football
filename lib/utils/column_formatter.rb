9be40375def0ee68304bbc9840731ea9909c4940
Doug de Jesus <douglasdejesus92@gmail.com>
2021-09-30 20:06:09 -0400

    Take column name into account when deciding width

--- lib/utils/column_formatter.rb
+++ lib/utils/column_formatter.rb
@@ -3,6 +3,6 @@ module ColumnFormatter
   def self.decimal(column_name, sig_figs)
     { column_name => { display_method: lambda { |data|
                                          format("%.#{sig_figs}f", data[column_name]).rjust(sig_figs + 3)
-                                       }, fixed_width: sig_figs + 3 } }
+                                       }, fixed_width: [sig_figs + 3, column_name.length].max } }
   end
 end
